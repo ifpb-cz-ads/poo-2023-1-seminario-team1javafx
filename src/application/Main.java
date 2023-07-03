7d76b373ebd5caa853d2f0265ede460f212710d0
Marcos Paulo <kingzabitus@gmail.com>
2023-07-03 20:50:54 -0300

    isso aí

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,11 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.stage.Stage;;
 
 
 public class Main extends Application {
-	@Override
+
 	public void start(Stage stage) {
 		try {
 			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
